ac64dfc2ae603df109b6a73b8df208298fb94dc5
Jeremy Zehr <jeremy.e.zehr@gmail.com>
2020-12-23 14:51:20 -0500

    Position elements absolutely

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Template
-Find templates for PennController experiments
+# Mouse Tracking
+Track mouse movements as participants click on one of four images while listening to an audio description
--- data_includes/main.js
+++ data_includes/main.js
@@ -1,19 +1,13 @@
-// This is a template for mouse tracker, feel free to edit it and adjust it
-// Find a tutorial and the list of availalbe elements at:
-// https://www.pcibex.net/documentation/
+PennController.ResetPrefix(null); // Shorten command names (keep this line here))
 
-PennController.ResetPrefix(null) // Shorten command names (keep this line here)
-
-// To turn off debugger uncomment the next line
-// DebugOff()
+// DebugOff()   // Uncomment this line only when you are 100% done designing your experiment
 
 // Show the consent first, then intro page with instructions
 // then all the 'experiment' trials in a random order, then send the results and finally show the trial labeled 'end'
-
 Sequence( "consent", "intro", randomize("experiment"), SendResults() , "end" )
 
-// Showing consent, generated throughout html file that you can edit 
 
+// Showing consent, stored in a html file that you can edit 
 newTrial ( "consent" ,
     defaultText
         .print()
@@ -27,125 +21,123 @@ newTrial ( "consent" ,
         .wait()
 )
 
-// Showing page with instructions, generated throughout html file that you can edit 
-// Futhermore, you have an option of changing audio of instruction, by uploading it 
-
+// Showing page with instructions, in a html file that you can edit 
+// The audio file was uploaded to Resources
 newTrial( "intro" ,
-    newHtml("intro_page", "intro.html")
+    newHtml("intro.html")
         .print()
     ,
-    newButton("<p>Continue.")
+    newButton("Continue.")
         .center()
         .print()
         .wait()
     ,
-    newAudio("instructAudio", "Instructions.mp3")
-        .play()
-    ,
-    newHtml("instruct_page", "AudioInstructions.html")
+    newHtml("AudioInstructions.html")
         .print()
     ,
-    newText("<p>When you understand these instructions, please click Continue.")
+    newText("When you understand these instructions, please click Continue.")
         .center()
         .print()
     ,
-    getAudio("instructAudio")
-        .wait("first")
+    newAudio("Instructions.mp3")
+        .play()
+        .wait()
     ,
-    newButton("<p>Continue.")
+    newButton("Continue")
         .center()
         .print()
         .wait()
 )
 
 
-
 // Starting the experiment, by using data from csv file we made previously
-
-Template ("extable.csv",
-row => newTrial("experiment",
-    newImage("1", row.image1)
-        .size(200,200)
+Template ("extable.csv", row => 
+  newTrial("experiment",
+    // All the images will have the same size: 200px*200px
+    defaultImage.size(200,200)
     ,
-    newImage("2", row.image2)
-        .size(200,200)
+    newCanvas("images", 1100, 500)  // 500px height: we will print the button at the bottom
+        .color("white")             // Use a white background
+        .add(  0 , 0 , newImage("1", row.image1) )
+        .add(300 , 0 , newImage("2", row.image2) )
+        .add(600 , 0 , newImage("3", row.image3) )
+        .add(900 , 0 , newImage("4", row.image4) )
+        .print("center at 50vw","top at 2em")
     ,
-    newImage("3", row.image3)
-        .size(200,200)
-    ,
-    newImage("4", row.image4)
-        .size(200,200)
-    ,
-    newCanvas("images", 1400, 200)
-        .center()
-        .add(350   , 0 , getImage("1") )
-        .add(650 , 0 , getImage("2") )
-        .add(850 , 0 , getImage("3") )
-        .add(1050, 0 , getImage("4") )
-        .print()
-    ,
-    newAudio("description", row.audiofile)
-    ,
-    newVar("isEarly", 0)
-    ,
+    // These are the feedback tooltips, in case the mouse moves too early or click is too late
     newTooltip("earlyWarning", "STARTED TOO EARLY. You moved your mouse from the Go button before it was possible to guess the correct option. Please don't move your mouse until you're about to click.")
-        .position("top center")
-    ,
-    newVar("slowClick", 0)
     ,
     newTooltip("slowClickWarning", "CLICKED TOO LATE. You took too long to click on your selection. Please try to click faster next time!")
-        .position("top center")
     ,
-    newTimer(2000) // 2000 ms to preview images
+    // Give 2s to preview the images
+    newTimer(2000)
         .start()
         .wait()
     ,
+    // This button is horizontally centered, at the bottom of the 1100px*500px surface:
+    // that's where the cursor will be when we start tracking the mouse
     newButton("Go")
-        .print( "center at 50vw" , "center at 90vh" )
+        .print( "center at 50%" , "bottom at 100%" , getCanvas("images") )
         .wait()
         .remove()
     ,
-    newTimer("earlyStart", (parseInt(row.NPTime) - 200) )
+    newVar("isEarly", false).global()   // Global to access it in newTrial().log
     ,
-    newTimer("timeLimit", (parseInt(row.audiodur) + 2000) )
+    newVar("slowClick", false).global() // Global to access it in newTrial().log 
     ,
+    // Launch timers to detect early movement and late clicks
+    newTimer("earlyStart", (parseInt(row.NPTime) - 200) ).start()
+    ,
+    newTimer("timeLimit", (parseInt(row.audiodur) + 2000) ).start()
+    ,
+    // Start tracking mouse movements and clicks
     newMouseTracker("mouse")
         .log()
-        .callback( getTimer("earlyStart").test.running().success(getVar("isEarly").set(1)) )
+        // Check the earlyStart timer whenever the mouse moves:
+        // set the "isEarly" Var element to true if the timer is still running
+        .callback( getTimer("earlyStart").test.running().success(getVar("isEarly").set(true)) )
         .start()
     ,
-    getAudio("description")
-        .play()
+    // Play the audio description
+    newAudio("description", row.audiofile).play()
     ,
-    getTimer("earlyStart")
-        .start()
-    ,
-    getTimer("timeLimit")
-        .start()
-    ,
+    // Make the images clickable
     newSelector()
         .add( getImage("1") , getImage("2") , getImage("3") , getImage("4"))
-        .callback( getTimer("timeLimit").test.ended().success(getVar("slowClick").set(1)) )
         .log()
         .wait()
     ,
-    getAudio("description")
-       .wait("first")
+    // If the "timeLimit" timer has ended by the time an image is clicked, set "slowClick" to true
+    getTimer("timeLimit").test.ended().success(getVar("slowClick").set(true))
     ,
-    getMouseTracker("mouse")
-        .stop()
+    // Make sure the description is done playing before proceeding
+    getAudio("description").wait("first")
     ,
-    getVar("isEarly")
-        .test.is(1).success(getTooltip("earlyWarning").print().wait())
+    // Stop the mouse tracker to avoid massive resuls files
+    getMouseTracker("mouse").stop()
     ,
-    getVar("slowClick")
-        .test.is(1).success(getTooltip("slowClickWarning").print().wait())
+    // Show feedback if necessary
+    getVar("isEarly").test.is(true).success(
+        getTooltip("earlyWarning")
+            .print( "center at 50%", "middle at 50%" , getCanvas("images"))
+            .wait()
+    )
+    ,
+    getVar("slowClick").test.is(true).success(
+        getTooltip("slowClickWarning")
+            .print( "center at 50%", "middle at 50%" , getCanvas("images"))
+            .wait()
+    )
   )
-  .log( "ID"     , getVar("ID")    )
-  .log( "Target"   , row.targetlocation  )
+  // Log the participant's id, passed as a parameter in the URL (?id=...)
+  .log( "id"                , GetURLParameter("id") )
+  // Log values from table and from Var elements
+  .log( "Target"            , row.targetlocation )
+  .log( "NPTime"            , row.NPTime )
+  .log( "audiodur"          , row.audiodur )
   .log( "EarlyStartMessage" , getVar("isEarly") )
-  .log( "TooSlowMessage" , getVar("slowClick") ) )
-
+  .log( "TooSlowMessage"    , getVar("slowClick") )
+)
 
 
 // The end of the experiment
@@ -154,11 +146,12 @@ newTrial("end",
         .center()
         .print()
     ,
+    // This is a dummy link, it won't actually validate submissions; use the link provided by your pooling platform
     newText("<p><a href='https://www.pcibex.net/' target='_blank'>Click here to validate your submission</a></p>")
         .center()
         .print()
     ,
-    newTimer(1)
-        .wait()
+    // Wait on this page forever
+    newButton().wait()
 )
 .setOption("countsForProgressBar",false)
